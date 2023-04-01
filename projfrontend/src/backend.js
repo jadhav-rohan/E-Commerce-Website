6e52dd614a68e20c3e2c29da138d9bc0180fb4c1
Rohan Jadhav <rohanjadhav@Rohans-MacBook-Air.local>
2023-04-01 12:40:17 +0530

    backend api

--- projfrontend/src/backend.js
+++ projfrontend/src/backend.js
@@ -1,2 +1,4 @@
 // export const API = process.env.REACT_APP_BACKEND;
-export const API = "http://localhost:8000/api/";
+// export const API = "http://localhost:8000/api/";
+export const API = "https://ecommerce-jryz.onrender.com";
+
